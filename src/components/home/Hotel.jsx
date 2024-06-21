348c58b186fa5d8afe8b7c4b0c872d23d2e4aaed
Bawantha <bawanthapasqual@gmail.com>
2024-06-21 14:03:55 +0530

    Hoteljson.jsx

--- src/components/home/Hotel.jsx
+++ src/components/home/Hotel.jsx
@@ -1,60 +1,55 @@
-import React, { useState } from 'react';
-import '../../assets/css/Hotel.css';
-import { useGetHotelList1Query, useGetHotelList2Query } from '../../store/api/hotelApi';
-import { Rate, SelectPicker } from 'rsuite';
-import HomeBtm from './HomeBtm';
-import Chatbot from '../common/Chatbot';
-import chatIcon from '../../assets/images/chatbot-removebg-preview.png';
+import React, { useState } from "react";
+import "../../assets/css/Hotel.css";
+import { Rate, SelectPicker } from "rsuite";
+import HomeBtm from "./HomeBtm";
+import Chatbot from "../common/Chatbot";
+import chatIcon from "../../assets/images/chatbot-removebg-preview.png";
+import HotelData from "../../assets/data/HotelData.json";
 
 function Hotel() {
-  const { data: hotelsData1, error: error1, isLoading: isLoading1 } = useGetHotelList1Query();
-  const { data: hotelsData2, error: error2, isLoading: isLoading2 } = useGetHotelList2Query();
   const [showChatbot, setShowChatbot] = useState(false);
 
   const toggleChatbot = () => {
     setShowChatbot(!showChatbot);
   };
 
-  if (isLoading1 || isLoading2) return <p>Loading...</p>;
-  if (error1 || error2) return <p>Error loading hotels</p>;
-
-  const combinedHotelsData = [...(hotelsData1 || []), ...(hotelsData2 || [])];
-
   return (
     <>
       <div className="hotel-main">
         <div className="hotel-main-top">
-          <div className="hotel-main-title">Hotels And Villas In Galle Area</div>
+          <div className="hotel-main-title">
+            Hotels And Villas In Galle Area
+          </div>
         </div>
         <div className="hotel-main-btm">
           <SelectPicker
             searchable={false}
-            style={{ width: 224, marginRight: '25px' }}
+            style={{ width: 224, marginRight: "25px" }}
             placeholder="Hotel or Villa"
             data={[]}
           />
           <SelectPicker
             searchable={false}
-            style={{ width: 224, marginRight: '25px' }}
+            style={{ width: 224, marginRight: "25px" }}
             placeholder="Room Type"
             data={[]}
           />
           <SelectPicker
             searchable={false}
-            style={{ width: 224, marginRight: '25px' }}
+            style={{ width: 224, marginRight: "25px" }}
             placeholder="Location"
             data={[]}
           />
           <SelectPicker
             searchable={false}
-            style={{ width: 224, marginRight: '25px' }}
+            style={{ width: 224, marginRight: "25px" }}
             placeholder="Price"
             data={[]}
           />
         </div>
         <div className="hotels">All Hotels In Galle Area</div>
         <div className="hotel-con">
-          {combinedHotelsData.map((hotel) => (
+          {HotelData.map((hotel) => (
             <div className="hotel-box-con" key={hotel.id}>
               <div className="hotel-box-con-top">
                 <img src={hotel.picture.url} alt="hotel" className="hotelimg" />
@@ -66,12 +61,14 @@ function Hotel() {
                   </div>
                   <div>
                     <Rate
-                      defaultValue={hotel.reviews?.count / 100 || 0}
+                      defaultValue={
+                        hotel.stars?.count || hotel.reviews?.count / 50
+                      }
                       style={{
-                        width: '80px',
-                        fontSize: '12px',
-                        marginBottom: '0px',
-                        color: 'black',
+                        width: "80px",
+                        fontSize: "12px",
+                        marginBottom: "0px",
+                        color: "black",
                       }}
                       allowHalf
                       readOnly
